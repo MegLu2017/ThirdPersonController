3e61e73657e209ad63953109df86a2e9da5a5deb
MegLu2017 <78464966+MegLu2017@users.noreply.github.com>
2021-02-24 10:56:52 -0500

    Added gravity and slowed animations

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -12,9 +12,23 @@ public class ThirdPersonController : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public float gravity = -9.81f;
+    Vector3 velocity;
+    bool isGrounded;
+
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y <0)
+        {
+            velocity.y = -2f;
+        }
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector2 direction = new Vector2(horizontal, vertical).normalized;
@@ -28,5 +42,8 @@ public class ThirdPersonController : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
