35b0409c76ddc731903049980d87343ceb83d891
MegLu2017 <78464966+MegLu2017@users.noreply.github.com>
2021-02-24 03:53:44 -0500

    Animations and Animation controller

---
+++ Assets/Scripts/AniStateCont.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AniStateCont : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector2 direction = new Vector2(horizontal, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+}
